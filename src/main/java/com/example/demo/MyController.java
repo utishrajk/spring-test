312825b826f51d937c2e6c273ed047ecbcd3cef2
utish <utishrajk@gmail.com>
2019-06-17 19:53:49 -0400

    change 2

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -3,11 +3,23 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class MyController {
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello Cruel World!!!";
+    }
+
+    @GetMapping("/hellos")
+    public List<String> hellos() {
+        List<String> list = new ArrayList<>();
+        list.add("Hello");
+        list.add("Nice");
+        list.add("World");
+        return list;
     }
 }
